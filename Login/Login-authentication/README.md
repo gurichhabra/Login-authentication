ffa4ede532e6f2338bebab036c287a47c1e286be
gurichhabra <104682130+gurichhabra@users.noreply.github.com>
2023-09-22 14:12:44 +0530

    Add files via upload

---
+++ Login/Login-authentication/README.md
@@ -0,0 +1,2 @@
+# Login-authentication
+Most basic login authentication with login jsp page, a login servlet to confirm and a welcome jsp. Can further improve it as required. 
---
+++ Login/src/main/java/cscorner/LoginServlet.java
@@ -0,0 +1,54 @@
+package cscorner;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import static java.lang.System.out;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/guri?useUnicode=true&character_set_server=utf8mb4\r\n"
+					+ "", "root", "bobby");
+			String n=request.getParameter("txtName");
+			String p=request.getParameter("txtPwd");
+			PreparedStatement ps=con.prepareStatement("select uname from login where uname=? and password=?");
+			ps.setString(1, n);
+			ps.setString(2, p);
+			ResultSet rs=ps.executeQuery();
+			if(rs.next())
+			{
+				RequestDispatcher rd=request.getRequestDispatcher("welcome.jsp");
+				rd.forward(request, response);
+			}
+			else
+			{
+				out.println("<font color= red size=18>Login failed!!<br>");
+				out.println("<a href=login.jsp>Try AGAIN!!</a>");
+			}
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
